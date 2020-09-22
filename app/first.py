7b8a72b5907fd1e2713b4b55ad46e326f8e2c68b
Ritik <ritikkushwaha1234@gmail.com>
2020-09-22 16:21:10 +0530

    My new project

--- app/first.py
+++ app/first.py
@@ -1,2 +1,63 @@
-print("Enter Your Name : ")
-print(input())
+""""""
+""" finding fraction"""
+# def frac(n):
+#     if n <= 0:
+#         return "INVALID"
+#     elif n == 1:
+#         return n
+#     else:
+#         return n * frac(n - 1)
+
+#
+# def finding(z):
+#     count = 0
+#     x = str(z)
+#     for i in range(0, len(x)):
+#         if x[-1 + i] == '0':
+#             count = count + 1
+#         else:
+#             print("E")
+#             break
+#     return count
+#
+#
+# num = int(input())
+# fac = frac(num)
+# print(finding(fac))
+
+""" finding prime """
+# def prime(z):
+#     x = []
+#     for i in range(1, z + 1):
+#         if z % (i) == 0:
+#             x.append(i)
+#     print(x[1])
+#
+#
+# test = int(input())
+# t = 0
+# while t < test:
+#     num = int(input())
+#     prime(num)
+#
+#     t = t + 1
+""" finding and replacing  'ghi ihk' to 'g_hik' """
+
+# int main() {
+# 	int num[100000],test,test2,i,t =0 ;
+# 	cin >> test;
+# 	while(t < test){
+# 	    int x=1,y = 0;
+# 		cin >> test2;
+# 		for(i=0;i<test2;i++){
+# 			cin >>num[i];
+# 			x = x*num[i];
+# 			y = y+num[i];
+# 		}
+# 		cout<<"Case #"<<t+1<<": "<<x/y<<endl;
+# 		t++;
+# 	}
+# }
+#
+
+
--- oops.py
+++ oops.py
@@ -1,8 +1,21 @@
-class Student:
-    collage_name = 'Hari Singh Collage Delhi'
-    pass
+# import pyttsx3
+# import requests
+#
+# engine = pyttsx3.init()
+# engine.say("My first code on text-to-speech")
 
+# cls_news = requests.get(
+#     'https://newsapi.org/v2/top-headlines?country=in&apiKey=8ce5b726e522439f8626ebb24c2aa4c4'
+#     '&category=sport&pageSize=5&q=ipl')
 
-rohan = Student()
-rohan.age = 10
-print(rohan.collage_name)
+params = input("Enter:> ").split(',')
+print(params)
+# parsed_news = json.loads(cls_news.text)
+# total_news = len(parsed_news['articles'])
+# print(total_news)
+#
+# for i in range(6):
+#     title = parsed_news['articles'][i]['title']
+#     print(title)
+#     engine.say(title)
+#     engine.runAndWait()

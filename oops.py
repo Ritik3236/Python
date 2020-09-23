6b7118b887e3593ba94b50a5a76bbe7f473cacf7
Ritik <ritikkushwaha1234@gmail.com>
2020-09-23 15:52:51 +0530

    My new project

--- oops.py
+++ oops.py
@@ -1,21 +1,22 @@
-# import pyttsx3
-# import requests
-#
-# engine = pyttsx3.init()
-# engine.say("My first code on text-to-speech")
+import json
 
-# cls_news = requests.get(
-#     'https://newsapi.org/v2/top-headlines?country=in&apiKey=8ce5b726e522439f8626ebb24c2aa4c4'
-#     '&category=sport&pageSize=5&q=ipl')
+import pyttsx3
+import requests
 
-params = input("Enter:> ").split(',')
-print(params)
-# parsed_news = json.loads(cls_news.text)
-# total_news = len(parsed_news['articles'])
-# print(total_news)
-#
-# for i in range(6):
-#     title = parsed_news['articles'][i]['title']
-#     print(title)
-#     engine.say(title)
-#     engine.runAndWait()
+engine = pyttsx3.init()
+
+url = 'https://newsapi.org/v2/top-headlines'
+params = {'country': 'in', 'category': 'sport', 'pageSize': '5', 'q': 'ipl'}
+cls_news = requests.get(url, params=params, headers={'X-Api-Key': '8ce5b726e522439f8626ebb24c2aa4c4'}, timeout=2)
+parsed_news = json.loads(cls_news.text)
+total_news = len(parsed_news['articles'])
+print(total_news)
+
+for i in range(5):
+    try:
+        title = parsed_news['articles'][i]['title']
+        print(title)
+        # engine.say(title)
+        # engine.runAndWait()
+    except:
+        print("good")

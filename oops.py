cc8a8dacb3ff56887df2751ceebed5e427a8878e
Ritik <ritikkushwaha1234@gmail.com>
2020-09-23 17:07:40 +0530

    My new project

--- oops.py
+++ oops.py
@@ -10,24 +10,20 @@ engine = pyttsx3.init()
 # news_item = input("Enter News_item : ")
 # q = input("Enter Topic : ")
 url = 'https://newsapi.org/v2/everything'
-
-# params = {'pageSize': 5, 'q': 'ipl', 'sortBy': 'relevancy'}
-params = {'country': 'in', 'category': 'sport', 'pageSize': '5', 'q': 'ipl', 'sortBy': 'relevancy'}
+params = {'pageSize': 10, 'q': ['Water'], 'sortBy': 'relevancy'}
+# params = {'country': 'in', 'category': 'sport', 'pageSize': '5', 'q': 'ipl', 'sortBy': 'relevancy'}
 
 cls_news = requests.get(url, params=params, headers={'X-Api-Key': '8ce5b726e522439f8626ebb24c2aa4c4'})
-var = cls_news.status_code
-if var == 200:
+if cls_news.status_code == 200:
     parsed_news = json.loads(cls_news.text)
-    print(parsed_news)
-
     for i in range(len(parsed_news['articles'])):
         try:
             title = parsed_news['articles'][i]['title']
-            print(title)
-            # engine.say(title)
-            # engine.runAndWait()
-        except:
-            print("good")
+            print(f"{i + 1}. {title}")
+            engine.say(title)
+            engine.runAndWait()
+        except KeyboardInterrupt:
+            print("ERROR")
 else:
     error = json.loads(cls_news.text)
-    print(error['code'], error['message'])
+    print(f"Status Code : {cls_news.status_code}\nError : {error['message']}")

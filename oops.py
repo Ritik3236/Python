6463dd26086cb77ff15559c552f231cc218f03bf
Ritik <ritikkushwaha1234@gmail.com>
2020-09-23 16:34:59 +0530

    My new project

--- oops.py
+++ oops.py
@@ -5,18 +5,29 @@ import requests
 
 engine = pyttsx3.init()
 
-url = 'https://newsapi.org/v2/top-headlines'
-params = {'country': 'in', 'category': 'sport', 'pageSize': '5', 'q': 'ipl'}
-cls_news = requests.get(url, params=params, headers={'X-Api-Key': '8ce5b726e522439f8626ebb24c2aa4c4'}, timeout=2)
-parsed_news = json.loads(cls_news.text)
-total_news = len(parsed_news['articles'])
-print(total_news)
+# country = input("Enter Country code : ")
+# # category = input("Enter Category : ")
+# news_item = input("Enter News_item : ")
+# q = input("Enter Topic : ")
+url = 'https://newsapi.org/v2/everything'
 
-for i in range(5):
-    try:
-        title = parsed_news['articles'][i]['title']
-        print(title)
-        # engine.say(title)
-        # engine.runAndWait()
-    except:
-        print("good")
+# params = {'pageSize': 5, 'q': 'ipl', 'sortBy': 'relevancy'}
+params = {'country': 'in', 'category': 'sport', 'pageSize': '5', 'q': 'ipl', 'sortBy': 'relevancy'}
+
+cls_news = requests.get(url, params=params, headers={'X-Api-Key': '8ce5b726e522439f8626ebb24c2aa4c4'})
+var = cls_news.status_code
+if var == 200:
+    parsed_news = json.loads(cls_news.text)
+    print(parsed_news)
+
+    for i in range(len(parsed_news['articles'])):
+        try:
+            title = parsed_news['articles'][i]['title']
+            print(title)
+            # engine.say(title)
+            # engine.runAndWait()
+        except:
+            print("good")
+else:
+    error = json.loads(cls_news.text)
+    print(error['code'], error['message'])
